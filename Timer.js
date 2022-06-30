a41cdd85e6d3cf84d43a335b086d6d538ffdf0a7
Phillip Chan <phillip@graise.io>
2022-06-30 20:25:19 +0800

    dev: add timer animation with js logic

--- Timer.js
+++ Timer.js
@@ -1,21 +1,26 @@
 export default class Timer {
-    constructor(duration, displayLocation) {
+    constructor(duration, displayLocation, clockObject) {
         this.defaultDuration = duration;
         this.duration = duration;
         this.displayLocation = displayLocation;
+        this.clockObject = clockObject;
+        this.clockObject.style.animationDuration = this.defaultDuration + "s";
         this.timerInterval = null;
     }
 
     setDuration(duration) {
-        this.stopTimer()
+        this.stopTimer();
         this.defaultDuration = duration;
         this.duration = duration;
+        console.log(this.defaultDuration + "s")
+        this.clockObject.style.animationDuration =
+            this.defaultDuration/1000 + "s";
     }
 
     display() {
-        let minute = Math.trunc(this.duration / 60 /1000);
-        let second = Math.trunc(this.duration % 60);
-
+        let minute = Math.trunc(this.duration / 60 / 1000);
+        let second = Math.trunc(this.duration /1000 % 60);
+        console.log(this.duration)
         this.displayLocation.innerHTML =
             (minute > 10 ? minute : "0" + minute) +
             ":" +
@@ -23,13 +28,15 @@ export default class Timer {
     }
 
     startTimer() {
+        this.clockObject.style.webkitAnimationPlayState = "running";
         this.timerInterval = setInterval(() => {
             if (this.duration > 0) {
-                this.duration--;
+                this.duration-=1000;
                 this.display();
             } else {
                 alert("Time up");
                 // TODO Create a Sound Alert
+                this.clockObject.style.webkitAnimationPlayState = "paused";
                 clearInterval(this.timerInterval);
             }
         }, 1000);
@@ -37,12 +44,16 @@ export default class Timer {
 
     stopTimer() {
         clearInterval(this.timerInterval);
+        this.clockObject.style.webkitAnimationPlayState = "paused";
         this.display();
     }
 
     resetTimer() {
         clearInterval(this.timerInterval);
         this.duration = this.defaultDuration;
+        this.clockObject.style.webkitAnimationPlayState = "paused";
+        this.clockObject.style.webkitAnimationDuration =
+            this.defaultDuration/1000 + "s";
         this.display();
     }
 }
